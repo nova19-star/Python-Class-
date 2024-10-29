5f0928214c748e5375257d70a8a5708f3a713338
Tushar <tushar20212005@gmail.com>
2024-10-29 22:19:40 +0530

    Initial commit

---
+++ arithmatic_operations.py
@@ -0,0 +1,55 @@
+# Arithmatic Operations 
+# Addition (+)
+a = 13
+b = 4
+c = a+b
+print(c)
+
+# Subtraction(-)
+a = 13
+b = 4
+c = a-b
+print(c)
+
+# Multiplication (*)
+a = 13
+b = 4
+c = a*b
+print(c)
+
+# Division (/)
+a = 13
+b = 4
+c = a/b
+print(c)
+
+# Floor Division or Integer Division (/)
+a = 13
+b = 4
+c = a//b
+print(c)
+
+# Modulus (%)
+a = 13
+b = 4
+c = a%b
+print(c)
+
+# Exponential (**)
+a = 13
+b = 4
+c = a**b
+print(c)
+
+# Parenthesis( )        First exicutes whats inside the bracket
+a = 3+(2**4)-6
+print(a)
+
+
+
+
+
+
+
+
+
